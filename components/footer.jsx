56a2629d82d0294a81fa20bf7ded5172c3de6961
Vishal Gautam <ivishalgautamm@gmail.com>
2024-12-20 15:47:56 +0530

    made more responsive

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,20 +1,31 @@
 "use client";
 
 import Logo from "./logo";
-import { Muted } from "./ui/typography";
+import { H4, H6, Muted } from "./ui/typography";
 import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { RiInstagramFill } from "react-icons/ri";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import Balancer from "react-wrap-balancer";
 
 export default function Footer() {
   return (
     <footer className="bg-primary-100 py-4 pt-8">
       <div className="container space-y-4">
         <div className="grid gap-8 lg:grid-cols-12">
-          <div className="col-span-4 rounded-3xl bg-secondary p-8"></div>
-          <div className="col-span-8 rounded-3xl bg-secondary p-8">
+          <div className="space-y-2 rounded-3xl bg-secondary p-6 text-center lg:col-span-4 lg:p-8">
+            <H4 className={"font-merriweather text-white"}>
+              Lorem, ipsum dolor.
+            </H4>
+            <Muted className="text-sm text-white">
+              <Balancer>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+                ratione expedita iusto, exercitationem perspiciatis ipsum.
+              </Balancer>
+            </Muted>
+          </div>
+          <div className="rounded-3xl bg-secondary p-6 lg:col-span-8 lg:p-8">
             <div className="items-start justify-between space-y-8 sm:flex sm:space-y-0">
               <div className="space-y-3">
                 <Logo className={"text-white"} />
@@ -39,7 +50,7 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="text-center text-sm font-medium">
+        <div className="text-center text-xs font-medium sm:text-sm">
           Copyright© {new Date().getFullYear()}. onePolicy. All Right Reserved.
         </div>
       </div>

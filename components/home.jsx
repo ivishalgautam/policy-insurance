b6d0c60ff027e4b1da9320e1df4ae7fdcd7a292b
Vishal Gautam <ivishalgautamm@gmail.com>
2024-12-23 10:43:33 +0530

    added mor performant using lazy loading

--- components/home.jsx
+++ components/home.jsx
@@ -1,40 +1,40 @@
 import dynamic from "next/dynamic";
 import HeroSkeleton from "./skeletons/hero";
-const Hero = dynamic(() => import("@/components/hero"), {
-  loading: () => <HeroSkeleton />,
-});
-const Insurances = dynamic(() => import("@/components/insurances"), {
-  loading: () => "loading...",
-});
-const WhyUs = dynamic(() => import("@/components/why-us"), {
-  loading: () => "loading...",
-});
-const Testimonials = dynamic(
-  () => import("@/components/testimonials").then((data) => data.Testimonials),
-  {
-    loading: () => "loading...",
-  },
+import { lazy, Suspense } from "react";
+const Hero = lazy(() => import("@/components/hero"));
+const Insurances = lazy(() => import("@/components/insurances"));
+const WhyUs = lazy(() => import("@/components/why-us"));
+const Testimonials = dynamic(() =>
+  import("@/components/testimonials").then((data) => data.Testimonials),
 );
-const ContactSection = dynamic(() => import("@/components/contact-section"), {
-  loading: () => "loading...",
-});
-const FAQ = dynamic(() => import("@/components/faq"), {
-  loading: () => "loading...",
-});
+const ContactSection = lazy(() => import("@/components/contact-section"));
+const FAQ = lazy(() => import("@/components/faq"));
 
 export default function Home() {
   return (
     <div className="bg-primary-300">
-      <Hero />
-      <Insurances />
-      <WhyUs />
-      <Testimonials />
-      <ContactSection />
-      <div className="bg-white">
-        <div className="mx-auto max-w-2xl py-10">
-          <FAQ />
+      <Suspense fallback={<HeroSkeleton />}>
+        <Hero />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Insurances />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <WhyUs />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Testimonials />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ContactSection />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="bg-white">
+          <div className="mx-auto max-w-2xl py-10">
+            <FAQ />
+          </div>
         </div>
-      </div>
+      </Suspense>
     </div>
   );
 }

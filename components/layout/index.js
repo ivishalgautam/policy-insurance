aa64995a546c4fb444ca2562055a0403401e8f24
Vishal Gautam <ivishalgautamm@gmail.com>
2024-12-23 11:02:54 +0530

    added more performance using lazy loading

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,14 +1,18 @@
-import React from "react";
-import Navbar from "../../components/navbar";
-import Footer from "../footer";
+import React, { lazy, Suspense } from "react";
+const Navbar = lazy(() => import("../../components/navbar"));
+const Footer = lazy(() => import("../footer"));
 import { ScrollArea } from "../ui/scroll-area";
 
 export default function Layout({ children }) {
   return (
     <ScrollArea className="h-screen">
-      <Navbar />
+      <Suspense fallback={<div>loading...</div>}>
+        <Navbar />
+      </Suspense>
       {children}
-      <Footer />
+      <Suspense fallback={<div>loading...</div>}>
+        <Footer />
+      </Suspense>
     </ScrollArea>
   );
 }
